fe62a863ccbf2a57caf6e2dbb93bc36bd68cf380
FelipeAFV <felipeafigueroav@gmail.com>
2022-10-02 10:46:14 -0300

    Cambio en extraccion de cookie

--- src/auth/middlewares/check-auth.js
+++ src/auth/middlewares/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
     try {
         const cookie = req.cookies['jwt'];
-        const token = cookie.split(' ')[1];
+
+        const token = (cookie.split(' ').length > 1) ? cookie.split(' ')[1] : cookie;
         const payload = jwt.verify(token, process.env.SECRET_KEY);
         req.user = payload;
         next();
