419dad298367c833be41deb916de20b70ea4a130
FelipeAFV <felipeafigueroav@gmail.com>
2022-10-16 15:47:23 -0300

    Se añade sql para nuevo perfil y permisos

--- src/database/populate.sql
+++ src/database/populate.sql
@@ -9,12 +9,13 @@ insert into centros_costo(nombre) values('bienestar');
 insert into centros_costo(nombre) values('farmacia_municipal');
 
 
-insert into perfiles(id, nombre) values(1, 'admin');
+insert into perfiles(id, nombre) values(1, 'director');
 insert into perfiles(id, nombre) values(2, 'jefe_compra');
 insert into perfiles(id, nombre) values(3, 'gestor_compra');
 insert into perfiles(id, nombre) values(4, 'juridico');
 insert into perfiles(id, nombre) values(5, 'referente');
 insert into perfiles(id, nombre) values(6, 'solicitante');
+insert into perfiles(id, nombre) values(7, 'admin');
 
 insert into financiamientos(nombre) values('convenio');
 insert into financiamientos(nombre) values('presupuesto_das');
@@ -39,6 +40,10 @@ insert into permisos(id, nombre) values(1, 'SOPI_EDITAR_ESTADO_RECHAZADA');
 insert into permisos(id, nombre) values(2, 'SOPI_EDITAR_ESTADO_REVISION_REFERENTE');
 insert into permisos(id, nombre) values(3, 'SOPI_EDITAR_ESTADO_REVISADO_REFERENTE');
 insert into permisos(id, nombre) values(4, 'SOPI_EDITAR_ESTADO_EN_GESTION');
+insert into permisos(id, nombre) values(5, 'USUARIO_INGRESAR');
+insert into permisos(id, nombre) values(6, 'USUARIO_EDITAR');
+insert into permisos(id, nombre) values(7, 'USUARIO_ACTUALIZAR');
+insert into permisos(id, nombre) values(8, 'USUARIO_ELIMINAR');
 
 
 insert into accesos(perfil_id, permiso_id) values(5,3);
@@ -46,5 +51,15 @@ insert into accesos(perfil_id, permiso_id) values(1,1);
 insert into accesos(perfil_id, permiso_id) values(1,2);
 insert into accesos(perfil_id, permiso_id) values(1,3);
 insert into accesos(perfil_id, permiso_id) values(1,4);
+
+insert into accesos(perfil_id, permiso_id) values(1,5);
+insert into accesos(perfil_id, permiso_id) values(1,6);
+insert into accesos(perfil_id, permiso_id) values(1,7);
+insert into accesos(perfil_id, permiso_id) values(1,8);
+
+insert into accesos(perfil_id, permiso_id) values(7,5);
+insert into accesos(perfil_id, permiso_id) values(7,6);
+insert into accesos(perfil_id, permiso_id) values(7,7);
+insert into accesos(perfil_id, permiso_id) values(7,8);
 
 insert into usuarios(usuario, contrasena, nombre, apellido, perfil_id, creacion, ultima_actualizacion) values('admin', '$2b$05$MJLV7V5v9ZsCzytVt/ElneN16ZFnVG1/31ZJ3b4iYVrt.Wa.Rax2O', 'admin', 'admin', 1, '2022-10-10', '2022-10-10');
