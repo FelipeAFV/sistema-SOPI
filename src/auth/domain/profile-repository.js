a237f0dc5bea46f469da24ec3781943c996d1b45
Samuel Borraccini <samuel@MacBook-Air-de-Samuel.local>
2022-11-26 20:20:55 -0300

    ruta para consultar perfiles y sus permisos

--- src/auth/domain/profile-repository.js
+++ src/auth/domain/profile-repository.js
@@ -11,9 +11,25 @@ const findAllProfile = async () => {
     
 }
 
+const findProfileById = async (profileId) => {
+    try {
+        const profile = await Profile.findOne({where:{id:profileId}});
+        const permissions = await profile.getPermissions();
+        const permisos = [];
+        permissions.forEach(e => {
+            permisos.push({permisoId: e.id, nombrePermiso:e.name})
+        });
+        profile.setDataValue('permisos', permisos)
+        return profile;
+    } catch (e) {
+        return null;
+    }
+    
+}
 const findProfileFromUser = async (userId) => {
     return await Profile.findOne({ include: [{model: User, where: { id: userId}}]});
 } 
 
+exports.findProfileById = findProfileById;
 exports.findAllProfile = findAllProfile;
 exports.findProfileFromUser = findProfileFromUser;
--- src/auth/infraestructure/profile-controller.js
+++ src/auth/infraestructure/profile-controller.js
@@ -1,18 +1,36 @@
 const {sendHttpResponse} = require('../../share/utils/response-parser')
-const {findAllProfile} = require('../domain/profile-repository')
+const {findAllProfile, findProfileById} = require('../domain/profile-repository')
 
 
 const getAllProfile = async (req,res) => {
-    const profiles = await findAllProfile();
 
-    if(!profiles) {
-        sendHttpResponse(res, 'Error al buscar perfiles', 400, 'Error al buscar perfiles');
-        return;
+    const {profileId} = req.params;
+    if(!profileId){
+        const profiles = await findAllProfile();
+        if(!profiles) {
+            sendHttpResponse(res, 'Error al buscar perfiles', 400, 'Error al buscar perfiles');
+            return;
+        }else {
+            sendHttpResponse(res, profiles, 200);
+            return;
+        }
     }else {
-        sendHttpResponse(res, profiles, 200);
-        return;
+        const profile = await findProfileById(profileId)
+        if(!profile) {
+            sendHttpResponse(res, 'Error al buscar perfiles', 400, 'Error al buscar perfiles');
+            return;
+        }else {
+            sendHttpResponse(res, profile, 200);
+            return;
+        }
     }
+
+    
+
+    
 }
+
+
 
 
 exports.getAllProfile = getAllProfile;
