c033f11f1cd7453e877a29ab3ea236661309b5f8
Samuel Borraccini <samuel@MacBook-Air-de-Samuel.local>
2022-12-01 15:13:01 -0300

    ticket con estado

--- src/management/domain/ticket-repository.js
+++ src/management/domain/ticket-repository.js
@@ -43,7 +43,7 @@ const updateFromIdTicket = async (id, updateValues) => {
 }
 
 const getAllTickets = async(conditions, page ,perPage) => {
-    const tickets = await Ticket.findAndCountAll({where:conditions,include:[{model:User, attributes:['username','firstname','lastname']}] , offset: (page-1)*perPage, limit:perPage, distinct: true,order: [['fecha_creacion','ASC'] ]});
+    const tickets = await Ticket.findAndCountAll({where:conditions,include:[{model:User, attributes:['username','firstname','lastname']},{model:TicketStatus}] , offset: (page-1)*perPage, limit:perPage, distinct: true,order: [['fecha_creacion','ASC'] ]});
     
     return tickets;
 }
